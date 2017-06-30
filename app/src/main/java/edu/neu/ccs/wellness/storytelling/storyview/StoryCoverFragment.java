d2c54e7f364d059c75afb2a8185e0931f6861a90
baharsheikhi <haji-sheikhi.b@husky.neu.edu>
2017-06-30 13:37:49 -0400

    modified the storycoverfragment to have a photo and title

--- app/src/main/java/edu/neu/ccs/wellness/storytelling/storyview/StoryCoverFragment.java
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/storyview/StoryCoverFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import edu.neu.ccs.wellness.storytelling.R;
@@ -15,6 +17,8 @@ import edu.neu.ccs.wellness.storytelling.R;
  */
 public class StoryCoverFragment extends Fragment {
     private static final String STORY_TEXT_FACE = "fonts/pangolin_regular.ttf";
+    private static TextView storyTitle;
+    private static ImageView storyCover;
 
 
     public StoryCoverFragment() {
@@ -23,10 +27,12 @@ public class StoryCoverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_story_cover, container, false);
-        setContentText(view, getString(R.string.reflection_text));
+        View rootview = inflater.inflate(R.layout.fragment_story_cover, container, false);
+        //setContentText(view, getString(R.string.reflection_text));
+        this.storyTitle = (TextView) rootview.findViewById(R.id.text);
+        this.storyCover = (ImageView) rootview.findViewById(R.id.storyImage);
 
-        return view;
+        return rootview;
     }
 
     /***
