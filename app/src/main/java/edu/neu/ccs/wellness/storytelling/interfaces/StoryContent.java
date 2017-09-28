c53a9a3918c37020e58f92d95ee248da377371e2
Herman Saksono <herman.saksono@gmail.com>
2017-09-28 14:19:07 -0400

    added an updated ContentTypes and story.json

--- app/src/main/java/edu/neu/ccs/wellness/storytelling/interfaces/StoryContent.java
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/interfaces/StoryContent.java
@@ -4,12 +4,38 @@ import android.content.Context;
 
 /**
  * Created by hermansaksono on 6/13/17.
+
+ContentType is an enum, that is either:
+ -- COVER              : Show cover page, contains image and book title
+ -- PAGE               : Show each storybook's page, contains page image and 
+                         the narration text
+ -- REFLECTION_START   : Shows the reflection start screen (By default use 
+                         string/reflection_start_title and 
+                         reflection_start_subtitle)
+ -- REFLECTION         : Show the reflection screen. Contains text and subtext
+ -- STATEMENT          : Show the closing statement after a reflection(s). 
+                         Contains text and subtext.
+ -- CHALLENGE_INFO     : Show the challenge start screen (By default use
+                         string/challenge_info_title and 
+                         string/challenge_info_subtitle)
+  -- CHALLENGE         : Show the challenge selection page. Details TBD (By default use
+                         string/challenge_title and 
+                         string/challenge_subtitle)
+  -- CHALLENGE_SUMMARY : Show the summary of the challenge that has been 
+                         picked. Details TBD (By default use 
+						 string/challenge_summary_info and 
+                         string/challenge_summary_subtitle)
+  -- GENERIC           : Just show text and subtext
+  -- OTHER             : Do not show anything
  */
+
 
 public interface StoryContent {
 
     public enum ContentType {
-        COVER, PAGE, REFLECTION, STATEMENT, CHALLENGE, OTHER;
+        COVER, PAGE, REFLECTION_START, REFLECTION, STATEMENT, 
+		CHALLENGE_INFO, CHALLENGE, CHALLENGE_SUMMARY,
+		GENERIC, OTHER;
     }
 
     public int getId();
