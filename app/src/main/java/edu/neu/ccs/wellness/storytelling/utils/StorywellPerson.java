4647e031fa0cf052cba4b7399e43f085024bc0b2
Herman Saksono <herman.saksono@gmail.com>
2018-07-20 15:24:40 -0400

    add feature to save last sync time

--- app/src/main/java/edu/neu/ccs/wellness/storytelling/utils/StorywellPerson.java
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/utils/StorywellPerson.java
@@ -3,19 +3,19 @@ package edu.neu.ccs.wellness.storytelling.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
+import java.sql.Timestamp;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import edu.neu.ccs.wellness.miband2.model.MiBandProfile;
 import edu.neu.ccs.wellness.miband2.model.UserInfo;
 import edu.neu.ccs.wellness.people.Group;
 import edu.neu.ccs.wellness.people.Person;
 import edu.neu.ccs.wellness.people.PersonDoesNotExistException;
-import edu.neu.ccs.wellness.storytelling.Storywell;
 import edu.neu.ccs.wellness.storytelling.settings.Keys;
-import edu.neu.ccs.wellness.utils.WellnessIO;
 
 /**
  * Created by hermansaksono on 7/19/18.
@@ -56,6 +56,29 @@ public class StorywellPerson {
     public MiBandProfile getBtProfile() { return this.btProfile; }
 
     public UserInfo getBtUserInfo() { return this.btUserInfo; }
+
+    public GregorianCalendar getLastSyncTime(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        GregorianCalendar cal = (GregorianCalendar) GregorianCalendar.getInstance();
+        int time = getDefaultDate();
+        if (this.person.isRole(Person.ROLE_PARENT)) {
+            time = getInt(prefs, Keys.CAREGIVER_LAST_SYNC_TIME, String.valueOf(getDefaultDate()));
+        } else if (this.person.isRole(Person.ROLE_CHILD)) {
+            time = getInt(prefs, Keys.CHILD_LAST_SYNC_TIME, String.valueOf(getDefaultDate()));
+        }
+        Timestamp timestamp = new Timestamp(time);
+        cal.setTime(timestamp);
+        return cal;
+    }
+
+    public void setLastSyncTime(Context context, GregorianCalendar calendar) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (this.person.isRole(Person.ROLE_PARENT)) {
+            prefs.edit().putLong(Keys.CAREGIVER_LAST_SYNC_TIME, (int) calendar.getTimeInMillis());
+        } else if (this.person.isRole(Person.ROLE_CHILD)) {
+            prefs.edit().putLong(Keys.CHILD_LAST_SYNC_TIME, (int) calendar.getTimeInMillis());
+        }
+    }
 
     /* HELPER METHODS */
     private static Person getPersonByRole(Group group, String roleString) {
@@ -123,6 +146,19 @@ public class StorywellPerson {
         Calendar cal = Calendar.getInstance();
         int thisYear = cal.get(Calendar.YEAR);
         return thisYear - birthYear;
+    }
+
+    private static int getDefaultDate() {
+        GregorianCalendar calendar = (GregorianCalendar) Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getDefault());
+        calendar.set(Calendar.YEAR, 2018);
+        calendar.set(Calendar.MONTH, Calendar.JULY);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return (int) calendar.getTimeInMillis();
     }
 
 }
