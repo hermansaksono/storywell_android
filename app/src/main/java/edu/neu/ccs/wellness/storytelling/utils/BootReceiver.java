082daa44b642cd59bb696d83a747ab53a085dcac
Herman Saksono <herman.saksono@gmail.com>
2019-02-07 13:00:52 -0500

    fix a bug in the BootReceiver

--- app/src/main/java/edu/neu/ccs/wellness/storytelling/utils/BootReceiver.java
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/utils/BootReceiver.java
@@ -3,8 +3,12 @@ package edu.neu.ccs.wellness.storytelling.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
+import edu.neu.ccs.wellness.storytelling.Storywell;
 import edu.neu.ccs.wellness.storytelling.notifications.RegularReminderReceiver;
+import edu.neu.ccs.wellness.storytelling.settings.SynchronizedSetting;
+import edu.neu.ccs.wellness.storytelling.settings.SynchronizedSettingRepository;
 
 /**
  * Created by hermansaksono on 2/5/19.
@@ -14,9 +18,20 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d("SWELL", "Staring BootReceiver");
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            // TODO this may crash if the user logged out from Storywell
+            scheduleRegularReminders(context);
+        }
+    }
+
+    private static void scheduleRegularReminders(Context context) {
+        Storywell storywell = new Storywell(context);
+        if (storywell.userHasLoggedIn()) {
             RegularReminderReceiver.scheduleRegularReminders(context);
+
+            SynchronizedSetting setting = storywell.getSynchronizedSetting();
+            setting.setRegularReminderSet(true);
+            SynchronizedSettingRepository.saveLocalAndRemoteInstance(setting, context);
         }
     }
 
