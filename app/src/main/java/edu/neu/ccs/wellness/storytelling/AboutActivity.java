054d5ed77c16509368edbac02e54eda636fa12f6
Herman Saksono <hsaksono@ccs.neu.edu>
2019-03-08 14:01:20 -0500

    make setting accessible from About

--- app/src/main/java/edu/neu/ccs/wellness/storytelling/AboutActivity.java
+++ app/src/main/java/edu/neu/ccs/wellness/storytelling/AboutActivity.java
@@ -1,7 +1,11 @@
 package edu.neu.ccs.wellness.storytelling;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import edu.neu.ccs.wellness.storytelling.settings.SettingsActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        findViewById(R.id.button_setting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSettingActivity();
+            }
+        });
+    }
+
+    private void startSettingActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
