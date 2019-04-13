70e9a0fd604ff1af0a45eaf57a0dbed976388c92
Herman Saksono <hsaksono@ccs.neu.edu>
2019-04-13 00:19:06 -0400

    Add IndividualizedChallengeManager

--- app/src/main/java/edu/neu/ccs/wellness/fitness/interfaces/ChallengeManagerInterface.java
+++ app/src/main/java/edu/neu/ccs/wellness/fitness/interfaces/ChallengeManagerInterface.java
@@ -3,9 +3,12 @@ package edu.neu.ccs.wellness.fitness.interfaces;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.Map;
 
+import edu.neu.ccs.wellness.fitness.challenges.IndividualizedChallengesToPost;
 import edu.neu.ccs.wellness.fitness.challenges.UnitChallenge;
 import edu.neu.ccs.wellness.server.RestServer;
+import edu.neu.ccs.wellness.server.RestServer.ResponseType;
 
 /**
  * Created by hermansaksono on 2/5/18.
@@ -39,6 +42,17 @@ public interface ChallengeManagerInterface {
             throws IOException, JSONException;
 
     /**
+     * Get the a list of available challenges if the ChallengeStatus is either UNSTARTED or
+     * AVAILABLE. The challenges will be adjusted based on the given baseline map.
+     * @param peopleBaselineSteps
+     * @return Available challenges
+     * @throws IOException
+     * @throws JSONException
+     */
+    AvailableChallengesInterface getAvailableChallenges(Map<Integer, Integer> peopleBaselineSteps)
+            throws IOException, JSONException;
+
+    /**
      * Set the running challenge if the ChallengeStatus is AVAILABLE. Then sets the status to
      * UNSYNCED_RUN. This function MUST save the given challenge to a persistent storage.
      * It should not sync the given challenge to server.
@@ -69,12 +83,17 @@ public interface ChallengeManagerInterface {
      * Sync the running challenge (that was saved in the persistent storage) with the server.
      * @return The status of the synchronization
      */
-    RestServer.ResponseType syncRunningChallenge();
+    ResponseType syncRunningChallenge();
 
     /**
      *
      */
-    RestServer.ResponseType postUnitChallenge(UnitChallengeInterface unitChallenge);
+    ResponseType postUnitChallenge(UnitChallengeInterface unitChallenge);
+
+    /**
+     *
+     */
+    ResponseType postIndividualizedChallenge(IndividualizedChallengesToPost challengeToPost);
 
     void closeChallenge() throws IOException, JSONException;
 
